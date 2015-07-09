b868ae3b42c225fbda24bb5db1134be62bff5176
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2015-07-09 14:47:34 -0400

    Got the dropdown menu to work

--- src/app/main/submit.controller.js
+++ src/app/main/submit.controller.js
@@ -3,7 +3,14 @@
 
   angular
     .module('projects')
-    .controller('SubmitController', ['$firebase', '$scope'], function($scope, $firebase) {
+    .controller('SubmitController', ['$firebase', '$scope', '$http'], function($scope, $firebase, $http) {
+
+      var ref = new Firebase("https://mysycamore.firebaseio.com/");
+      // RestangularProvider.setBaseUrl('https://mysycamore.firebaseio.com/')
+
+      // Restangular.one('test').then(function(test) {
+      //   $scope.test = test;
+      //   console.log(test);
 
     });
 
