346e123a456132a7b81cb8d2e3db5a145c1ae7d5
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2015-07-10 19:32:17 -0400

    no errors, working controllers

--- src/app/main/submit.controller.js
+++ src/app/main/submit.controller.js
@@ -7,15 +7,15 @@
   /** @ngInject */
   function SubmitController($scope, $firebase) {
     var ref = new Firebase('https: //mysycamore.firebaseio.com')
-    $scope.relative = $firebase(ref);
-
-    $scope.addRelative = function(e) {
-      if (e.keycode != 13) return;
-
-      $scope.relative.$add({
-        name: $scope.name
-      });
-      $scope.newRelative = "";
-    }
+      // $scope.relative = $firebase(ref);
+      //
+      // $scope.addRelative = function(e) {
+      //   if (e.keycode != 13) return;
+      //
+      //   $scope.relative.$add({
+      //     name: $scope.name
+      //   });
+      //   $scope.newRelative = "";
+      // }
   }
 })();
