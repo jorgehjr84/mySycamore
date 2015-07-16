3f790c76327206406664a966359dfabcbdcea949
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2015-07-16 16:57:28 -0400

    Messing around with the links in the navbar

--- src/app/main/edit.controller.js
+++ src/app/main/edit.controller.js
@@ -5,14 +5,17 @@
 
 
   /** @ngInject */
-  function editController($scope, FIREBASE_URL, $firebaseObject, $stateParams, $http) {
-    //var cards = new Firebase('https://mysycamore.firebaseio.com/cards/');
+  function editController($scope, FIREBASE_URL, $firebaseObject, $stateParams, $http, $firebaseArray) {
+
     //$scope.result = $firebaseObject(cards.child($stateParams.card));
     $http.get('https://mysycamore.firebaseio.com/cards/' + $stateParams.card + '.json')
       .then(function(response) {
         $scope.card = response.data;
         // console.log(response);
       });
+
+
+
     console.log($stateParams);
 
 
