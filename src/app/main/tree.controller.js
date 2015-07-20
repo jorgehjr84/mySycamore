a998aa763c12786d717a1df11f2575864d6c0ea5
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2015-07-20 17:12:54 -0400

    Could not figure out positioning yet. Going to try to start assigning hardcoded styles

--- src/app/main/tree.controller.js
+++ src/app/main/tree.controller.js
@@ -10,13 +10,15 @@
 
     $http.get(FIREBASE_URL + '.json').success(function(data) {
       $scope.cards = data;
-    });
+      console.log($scope.cards);
 
+    });
     // $scope.deleteItem = function(id) {
     //   var itemRef = new Firebase(url + '/' + id);
     //   itemRef.remove();
     // }
     $scope.toggle = false;
+
 
     $scope.deleteCard = function(card) {
       var cardRef = new Firebase(FIREBASE_URL);
@@ -24,6 +26,25 @@
       $scope.cards.$remove(card);
       console.log(cardRef);
     };
+
+    // var moveCard = document.getElementById('moveCard');
+    // var container = document.getElementById('center_content');
+    //
+    // var cardLeft = 90;
+    //
+    // function positionCards(e) {
+    //   if (e.keycode == 39) {
+    //     left += 2;
+    //     moveCard.style.left = cardLeft + 'px';
+    //   }
+    //   if (e.keycode == 37) {
+    //     left -= 2;
+    //     moveCard.style.left = cardLeft + 'px';
+    //
+    //   }
+    //   // alert(e.keyCode);
+    // }
+    // document.onkeydown = positionCards;
 
   } //End of TreeController Function
 })();
