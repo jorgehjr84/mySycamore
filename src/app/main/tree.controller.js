7cf0f8182a363a1eb8cd85cbfc76eec30a9d4775
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2015-07-22 08:55:09 -0400

    Got the cards to be sortable, FOR REAL THIS TIME

--- src/app/main/tree.controller.js
+++ src/app/main/tree.controller.js
@@ -8,31 +8,39 @@
   /** @ngInject */
   function TreeController($scope, $http, $firebaseArray, FIREBASE_URL, $stateParams, $firebaseObject) {
 
-    $http.get(FIREBASE_URL + '.json').success(function(data) {
-      $scope.cards = data;
 
-    });
+    var cardsRef = new Firebase(FIREBASE_URL);
+    $scope.cards = $firebaseArray(cardsRef);
 
-    $scope.toggle = false;
-    $scope.toggle_grandParent = false;
-    $scope.toggle_parent = false;
-    $scope.toggle_spouse = false;
-    $scope.toggle_sibling = false;
-    $scope.toggle_child = false;
+    //  $http.get(FIREBASE_URL + '.json').success(function(data) {
+    //    $scope.cards = data;
+    //  });
+
+    // $scope.toggle = false;
+    // $scope.toggle_grandParent = false;
+    // $scope.toggle_parent = false;
+    // $scope.toggle_spouse = false;
+    // $scope.toggle_sibling = false;
+    // $scope.toggle_child = false;
 
 
-    $scope.deleteCard = function(id) {
-      $http.delete('https://mysycamore.firebaseio.com/cards/' + id + '.json');
-    };
+    // $scope.deleteCard = function(id) {
+    //   $http.delete('https://mysycamore.firebaseio.com/cards/' + id + '.json');
+    // };
 
 
     $scope.sortableOptions = {
-      update: function(e, ui) {},
+      update: function(e, ui) {
+
+      },
+      containment: "parent",
+      cursor: "move",
+
       axis: 'x'
     };
 
 
 
 
-  } //End of TreeController Function
+  }; //End of TreeController Function
 })();
